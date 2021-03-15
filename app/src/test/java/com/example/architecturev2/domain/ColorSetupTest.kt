09504495c50ee0977677d89b8afe4147ead673e4
ShymkoGleb <shymkogleb@gmail.com>
2021-03-16 00:42:42 +0200

    Start Test Branch with validation post TEST

---
+++ app/src/test/java/com/example/architecturev2/domain/ColorSetupTest.kt
@@ -0,0 +1,12 @@
+package com.example.architecturev2.domain
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class ColorSetupTest {
+
+    @Test
+    fun colorSetup() {
+    }
+}
